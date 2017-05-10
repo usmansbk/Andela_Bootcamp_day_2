91f344ad4db6dc5a056238b0a822646461022297
uxxi <usmansbk@gmail.com>
2017-05-10 17:05:55 +0100

    Fix Indentations

--- app/traffic.js
+++ app/traffic.js
@@ -3,43 +3,41 @@ var program = require('commander');
 var url = "https://www.tsaboin.com/api/statuses/public_timeline.json"
 
 function printData(body, range) {
-	for (var i = 0; i < range; i++) {
-		console.log("time: " + body[i]['created_at']);
-		console.log("info: " + body[i]['text']);
-		console.log("");
-	}
+    for (var i = 0; i < range; i++) {
+	console.log("time: " + body[i]['created_at']);
+	console.log("info: " + body[i]['text']);
+	console.log("");
+    }
 }
 
 function myreq(op) {
-	request({
-		url: url,
-		json: true
+    request({
+	url: url,
+	json: true
 	}, function (err, res, body) {
-		if (!err && res.statusCode === 200) {
-
-			switch (op) {
-				case 'l': {
-					printData(body, body.length);
-					break;
-				}
-				case 'h': {
-					printData(body, 10);
-					break;
-				}
+	       if (!err && res.statusCode === 200) {
+		    switch (op) {
+			case 'l': {
+			    printData(body, body.length);
+			    break;
 			}
-
+			case 'h': {
+			    printData(body, 10);
+			    break;
+			}
 		}
-	});
+	}
+    });
 }
 
 function listRoads(val) {
-	console.log("fetching list of roads");
-	myreq('l');
+    console.log("fetching list of roads");
+    myreq('l');
 }
 
 function getHeadData(val) {
-	console.log("Fetching " + val + " data");
-	myreq('h');
+    console.log("Fetching " + val + " data");
+    myreq('h');
 }
 
 program.version('0.0.1')
